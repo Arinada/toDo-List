d84120e04ffda6f3fcfa17dafa786d418fb9fc2d
Arinada <arina.potekhina@mail.ru>
2022-07-31 00:33:33 +0700

    feat: use double quotes

--- js/filter.js
+++ js/filter.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
-    $('.dropdown-item').click(function () {
+    $(".dropdown-item").click(function () {
         $(".filter-btn").text($(this).text())
     });
 
-    $('#search-btn').click(function () {
-        let filterValue = $('.filter-btn').text();
-        let searchValue = $('#search-value').val();
-        if(filterValue !== 'Filter') {
+    $("#search-btn").click(function () {
+        let filterValue = $(".filter-btn").text();
+        let searchValue = $("#search-value").val();
+        if(filterValue !== "Filter") {
             const urlParams = new URLSearchParams(window.location.search)
-            urlParams.set('filter', filterValue);
-            urlParams.set('value', searchValue);
+            urlParams.set("page", 1);
+            urlParams.set("filter", filterValue);
+            urlParams.set("value", searchValue);
             window.location.search = urlParams;
         }
     });
