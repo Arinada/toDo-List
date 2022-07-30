463e4eeacc1bce71880de8f9de79d4ce4fc106ad
Arinada <arina.potekhina@mail.ru>
2022-07-31 00:35:05 +0700

    feat: add parameters to get number of tasks by filter

--- models/Task.php
+++ models/Task.php
@@ -6,8 +6,49 @@ use mysqli_result;
 
 class Task
 {
+    private string $selectQuery = "SELECT * FROM Task INNER JOIN Author ON Author.id = Task.author_id";
+    private string $countQuery = "SELECT count(*) as task_number FROM Task INNER JOIN Author ON Author.id = Task.author_id";
 
-    public function getAllTasks(): array
+    public function getNumberOfAllTasks(): int
+    {
+        $stmt = DBClass::getConnection()->prepare($this->countQuery);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $this->getDataByResult($result)[0]['task_number'];
+    }
+
+    public function getNumberOfTasksByDescription(string $value): int
+    {
+        $query =  $this->countQuery . " WHERE description LIKE ?";
+        $param = "%$value%";
+        $result = $this-> getQueryWithParamResult($query, "s", $param);
+        return $this->getDataByResult($result)[0]['task_number'];
+    }
+
+    public function  getNumberOfTasksByStatus(int $status): int
+    {
+        $query =  $this->countQuery . " WHERE status=?";
+        $result = $this-> getQueryWithParamResult($query, "i", $status);
+        return  $this->getDataByResult($result)[0]['task_number'];
+    }
+
+    public function  getNumberOfTasksByAuthorName(string $value): int
+    {
+        $query =  $this->countQuery .  " WHERE Author.name LIKE ?";
+        $param = "%$value%";
+        $result = $this-> getQueryWithParamResult($query, "s", $param);
+        return  $this->getDataByResult($result)[0]['task_number'];
+    }
+
+    public function  getNumberOfTasksByEmail(string $value): int
+    {
+        $query =  $this->countQuery . " WHERE Author.email LIKE ?";
+        $param = "%$value%";
+        $result = $this-> getQueryWithParamResult($query, "s", $param);
+        return  $this->getDataByResult($result)[0]['task_number'];
+    }
+
+    public function getAllTasks(int $limitTaskNumberFrom, int $limitTaskNumberTo): array
     {
         $query = "SELECT * FROM Task INNER JOIN Author ON Author.id = Task.author_id";
         $stmt = DBClass::getConnection()->prepare($query);
