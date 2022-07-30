b40d2039d7940a2309e6b1952667cbe1240069f4
Arinada <arina.potekhina@mail.ru>
2022-07-31 00:31:54 +0700

    feat: add pagination

--- controllers/MainPage.php
+++ controllers/MainPage.php
@@ -9,6 +9,7 @@ use App\Views\View;
 class MainPage
 {
     private string $viewName;
+    private const TASKS_NUMBER_BY_PAGE = 3;
     private View $view;
     private const  STATUS_CODES = [
             'done' => 1,
@@ -26,10 +27,24 @@ class MainPage
     {
         $context = [];
         $taskModel = new Task();
-        $tasks = $this->getTasksByFilter($taskModel, $params['filter'] ?? '', $params['value'] ?? '');
+        $filter = $_GET['filter'];
+        $value = $_GET['value'];
+        $pageNumber = $_GET['page'] ?? 1;
+        $tasks = [];
+        $numberOfTasks = 0;
+
+        $limitTaskNumberFrom = self::TASKS_NUMBER_BY_PAGE * ($pageNumber - 1);
+        $limitTaskNumberTo = self::TASKS_NUMBER_BY_PAGE * $pageNumber;
+
+        if (!empty($value)) {
+            $numberOfTasks = $this->getTasksNumberByFilter($taskModel, $filter,  $value);
+            $tasks = $this->getTasksByFilter($taskModel, $filter, $value, $limitTaskNumberFrom, $limitTaskNumberTo);
+        }
+        $context['pages_number'] = $this->divRoundUp($numberOfTasks, self::TASKS_NUMBER_BY_PAGE);
+
 
         foreach ($tasks as $index => $task) {
-             $context[$index] = [
+             $context['tasks'][$index] = [
                  'author_name' => $task['name'],
                  'author_email' => $task['email'],
                  'description' => $task['description'],
@@ -40,21 +55,53 @@ class MainPage
         DBClass::getConnection()->close();
     }
 
-    private function getTasksByFilter(Task $taskModel,string $filter,string $value): array
+    private function divRoundUp(int $dividend,int $divider): int
+    {
+        if ($dividend === 0) {
+            return 1;
+        }
+        $result = (int)($dividend / $divider);
+        $remainder = $dividend % $divider;
+        if ($remainder > 0) {
+            return $result + 1;
+        }
+        return $result;
+    }
+
+    private function getTasksNumberByFilter(Task $taskModel,string $filter,string $value): int
     {
         switch ($filter) {
             case 'Description':
-                return $taskModel->getTasksByDescription($value);
+                return $taskModel->getNumberOfTasksByDescription($value);
             case 'Status':
-                $status = self::STATUS_CODES[$value] ?? 0;
-                return $taskModel->getTasksByStatus($status);
+                $status = self::STATUS_CODES[$value] ?? -1;
+                return $taskModel->getNumberOfTasksByStatus($status);
             case "Author name":
-                return $taskModel->getTasksByAuthorName($value);
+                return $taskModel->getNumberOfTasksByAuthorName($value);
             case "Email":
-                return $taskModel->getTasksByEmail($value);
-            default:
-                return $taskModel->getAllTasks();
+                return $taskModel->getNumberOfTasksByEmail($value);
+            case "Filter":
+                return $taskModel->getNumberOfAllTasks();
         }
+        return 0;
+    }
+
+    private function getTasksByFilter(Task $taskModel,string $filter,string $value, int $limitTaskNumberFrom, int $limitTaskNumberTo): array
+    {
+        switch ($filter) {
+            case 'Description':
+                return $taskModel->getTasksByDescription($value, $limitTaskNumberFrom, $limitTaskNumberTo);
+            case 'Status':
+                $status = self::STATUS_CODES[$value] ?? -1;
+                return $taskModel->getTasksByStatus($status, $limitTaskNumberFrom, $limitTaskNumberTo);
+            case "Author name":
+                return $taskModel->getTasksByAuthorName($value, $limitTaskNumberFrom, $limitTaskNumberTo);
+            case "Email":
+                return $taskModel->getTasksByEmail($value, $limitTaskNumberFrom, $limitTaskNumberTo);
+            case "Filter":
+                return $taskModel->getAllTasks($limitTaskNumberFrom, $limitTaskNumberTo);
+        }
+        return [];
     }
 
 }
